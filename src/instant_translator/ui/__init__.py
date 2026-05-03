bbd6c60154b34a0a627285784c8e749599b1c0d7
SuperFish1203 <66671142+SuperFish1203@users.noreply.github.com>
2026-05-03 11:59:51 +0800

    Add src\instant_translator\ui\__init__.py

---
+++ src/instant_translator/ui/__init__.py
@@ -0,0 +1,5 @@
+from instant_translator.ui.main_window import MainWindow
+from instant_translator.ui.popup_window import PopupWindow
+from instant_translator.ui.tray import AppTrayIcon
+
+__all__ = ["AppTrayIcon", "MainWindow", "PopupWindow"]
