562b8aa7ac98988db2118498c2a7a92b5c8a6cd3
SuperFish1203 <66671142+SuperFish1203@users.noreply.github.com>
2026-05-03 11:59:43 +0800

    Add src\instant_translator\translation\__init__.py

---
+++ src/instant_translator/translation/__init__.py
@@ -0,0 +1,4 @@
+from instant_translator.translation.base import BaseTranslator, TranslationResult
+from instant_translator.translation.manager import TranslatorManager
+
+__all__ = ["BaseTranslator", "TranslationResult", "TranslatorManager"]
